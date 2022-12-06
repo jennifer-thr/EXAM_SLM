0f9db2436aae5bbf287b86dca81dd9c68502cc3e
jenny <ic20b034@technikum-wien.at>
2022-12-06 19:46:18 +0100

    added repository

---
+++ src/main/java/fhtw/inch_to_centimeter_yard_to_meter/InchToCentimeterYardToMeterApplication.java
@@ -0,0 +1,13 @@
+package fhtw.inch_to_centimeter_yard_to_meter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InchToCentimeterYardToMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InchToCentimeterYardToMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhtw/inch_to_centimeter_yard_to_meter/InchToCentimeterYardToMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.inch_to_centimeter_yard_to_meter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InchToCentimeterYardToMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
